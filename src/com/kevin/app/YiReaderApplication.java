aee03550a0788be4cd3630700f57f399d7549ca1
kevin chen <chenyi_de_email@163.com>
2014-07-25 00:19:48 +0800

    增加百度云推送库

---
+++ src/com/kevin/app/YiReaderApplication.java
@@ -0,0 +1,12 @@
+package com.kevin.app;
+
+import com.baidu.frontia.FrontiaApplication;
+
+public class YiReaderApplication extends FrontiaApplication {
+
+	@Override
+	public void onCreate() {
+
+		super.onCreate();
+	}
+}
