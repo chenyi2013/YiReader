956105cdb05e46f5f38712f8a4425d3420b40355
kevin chen <chenyi_de_email@163.com>
2014-07-25 12:05:43 +0800

    实现百度推送功能

--- src/com/kevin/yireader/MainActivity.java
+++ src/com/kevin/yireader/MainActivity.java
@@ -1,36 +1,69 @@
 package com.kevin.yireader;
 
+import com.baidu.android.pushservice.CustomPushNotificationBuilder;
+import com.baidu.android.pushservice.PushConstants;
+import com.baidu.android.pushservice.PushManager;
 import android.support.v7.app.ActionBarActivity;
+import android.app.Notification;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
 public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Utils.logStringCache = Utils.getLogText(getApplicationContext());
 
+		if (!Utils.hasBind(getApplicationContext())) {
+			PushManager.startWork(getApplicationContext(),
+					PushConstants.LOGIN_TYPE_API_KEY,
+					Utils.getMetaValue(MainActivity.this, "api_key"));
+			// Push: 如果想基于地理位置推送，可以打开支持地理位置的推送的开关
+			// PushManager.enableLbs(getApplicationContext());
+		}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		Resources resource = this.getResources();
+		String pkgName = this.getPackageName();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		CustomPushNotificationBuilder cBuilder = new CustomPushNotificationBuilder(
+				getApplicationContext(), resource.getIdentifier(
+						"notification_custom_builder", "layout", pkgName),
+				resource.getIdentifier("notification_icon", "id", pkgName),
+				resource.getIdentifier("notification_title", "id", pkgName),
+				resource.getIdentifier("notification_text", "id", pkgName));
+		cBuilder.setNotificationFlags(Notification.FLAG_AUTO_CANCEL);
+		cBuilder.setNotificationDefaults(Notification.DEFAULT_SOUND
+				| Notification.DEFAULT_VIBRATE);
+		cBuilder.setStatusbarIcon(this.getApplicationInfo().icon);
+		cBuilder.setLayoutDrawable(resource.getIdentifier(
+				"simple_notification_icon", "drawable", pkgName));
+		PushManager.setNotificationBuilder(this, 1, cBuilder);
+		PushManager.startWork(getApplicationContext(),
+				PushConstants.LOGIN_TYPE_API_KEY,
+				Utils.getMetaValue(MainActivity.this, "api_key"));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
