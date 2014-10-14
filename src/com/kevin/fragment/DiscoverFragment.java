a62bc53c7cdf9415e86a64b85baeb3850710db8d
kevin chen <chenyi_de_email@163.com>
2014-10-14 23:46:15 +0800

    modify layout

--- src/com/kevin/fragment/DiscoverFragment.java
+++ src/com/kevin/fragment/DiscoverFragment.java
@@ -3,16 +3,20 @@ package com.kevin.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
+
 import com.kevin.yireader.R;
 
 public class DiscoverFragment extends Fragment {
-	
-	ViewPager v;
-	View viw;
+
+	private LinearLayout mPublicBookLayout;
 
 	public DiscoverFragment() {
 
@@ -34,6 +38,68 @@ public class DiscoverFragment extends Fragment {
 	@Override
 	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
+		mPublicBookLayout = (LinearLayout) getView().findViewById(
+				R.id.public_book_layout);
+		initView();
+	}
+
+	private void initView() {
+
+		// init public book layout one
+		LinearLayout layout = null;
+		for (int i = 0; i < 6; i++) {
+
+			if ((i) % 3 == 0) {
+
+				layout = new LinearLayout(getActivity());
+				LinearLayout.LayoutParams layoutParams = new LayoutParams(
+						LinearLayout.LayoutParams.MATCH_PARENT,
+						LinearLayout.LayoutParams.WRAP_CONTENT);
+				layout.setOrientation(LinearLayout.HORIZONTAL);
+				layout.setLayoutParams(layoutParams);
+				mPublicBookLayout.addView(layout);
+			}
+
+			TextView tv = new TextView(getActivity());
+			LinearLayout.LayoutParams tvLayoutParams = new LayoutParams(0,
+					LayoutParams.WRAP_CONTENT);
+			tvLayoutParams.weight = 1;
+			tvLayoutParams.setMargins(10, 10, 10, 10);
+			tv.setLayoutParams(tvLayoutParams);
+			tv.setText("流行小说");
+			tv.setGravity(Gravity.CENTER);
+			tv.setPadding(10, 10, 10, 10);
+			tv.setBackgroundColor(0xff78dfad);
+			layout.addView(tv);
+
+		}
+
+		// init public book layout two
+
+		for (int i = 0; i < 6; i++) {
+			if ((i) % 3 == 0) {
+
+				layout = new LinearLayout(getActivity());
+				LinearLayout.LayoutParams layoutParams = new LayoutParams(
+						LinearLayout.LayoutParams.MATCH_PARENT,
+						LinearLayout.LayoutParams.WRAP_CONTENT);
+				layout.setOrientation(LinearLayout.HORIZONTAL);
+				layout.setLayoutParams(layoutParams);
+				mPublicBookLayout.addView(layout);
+			}
+
+			LinearLayout linearLayout = (LinearLayout) getActivity()
+					.getLayoutInflater().inflate(R.layout.public_book_item,
+							layout, false);
+			LinearLayout.LayoutParams layoutParams = (LayoutParams) linearLayout
+					.getLayoutParams();
+			layoutParams.weight = 1;
+			layoutParams.width = 0;
+			linearLayout.setLayoutParams(layoutParams);
+			linearLayout.setPadding(10, 10, 10, 20);
+			layout.addView(linearLayout);
+
+		}
 	}
 
 }
